59d57ad848cfdafe09ba2df74f393b45595a3183
Gizem <gizemaltay0625@gmail.com>
2022-08-25 16:32:12 +0300

    .

--- src/Navigation/ComposeNavigation.js
+++ src/Navigation/ComposeNavigation.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import ComposeFirstPage from '../Pages/Compose/ComposeFirstPage';
-import ComposeSecondPage from '../Pages/Compose/ComposeSecond';
 const Stack = createNativeStackNavigator();
 
 const ComposeNavigation = () => (
@@ -9,7 +8,6 @@ const ComposeNavigation = () => (
     initialRouteName="ComposeFirstPage"
     screenOptions={{headerShown: false}}>
     <Stack.Screen name="ComposeFirstPage" component={ComposeFirstPage} />
-    <Stack.Screen name="ComposeSecondPage" component={ComposeSecondPage} />
   </Stack.Navigator>
 );
 
--- src/Pages/Compose/ComposeFirstPage.js
+++ src/Pages/Compose/ComposeFirstPage.js
@@ -9,12 +9,40 @@ import {
   ImageBackground,
   TouchableOpacity,
 } from 'react-native';
+import {io} from 'socket.io-client';
+
 import Image1 from '../../assets/manzara9.jpg';
 import {Dimensions} from 'react-native';
 import Background from '../../components/Common/Background';
+import {ScrollView} from 'react-native-gesture-handler';
 const {width, height} = Dimensions.get('window');
-const ComposeFirstPage = ({navigation}) => {
+let socket;
+
+const ComposeFirstPage = () => {
   const [userName, setUsername] = useState('');
+  const [message, setMessage] = useState('');
+  const [messageList, setMessageList] = useState([]);
+  const [isUserNameCreated, setUserNameCreated] = useState(false);
+
+  React.useEffect(() => {
+    if (!socket) {
+      socket = io('ws://192.168.1.92:5002');
+      console.log('new connection');
+      socket.on('connect', () => {
+        console.log(socket.id);
+      });
+
+      socket.on('event', data => {
+        console.log('new event', data);
+        messageList.push(data);
+        setMessageList([...messageList]);
+      });
+
+      socket.on('disconnect', () => {
+        console.log('onDisconnect');
+      });
+    }
+  }, []);
 
   return (
     <SafeAreaView>
@@ -27,21 +55,56 @@ const ComposeFirstPage = ({navigation}) => {
           <Background />
         </ImageBackground>
       </View>
-      <Text style={styles.chatext}>Chat ! </Text>
-      <View style={styles.ınputt}>
-        <TextInput
-          style={styles.textInput}
-          onChangeText={setUsername}
-          value={userName}
-        />
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() =>
-            navigation.navigate('ComposeSecondPage', {userName: userName})
-          }>
-          <Text style={styles.chatButton}>Gönder</Text>
-        </TouchableOpacity>
-      </View>
+      {!isUserNameCreated ? (
+        <View>
+          <Text style={styles.chatext}>Kullanıcı Adı </Text>
+          <View style={styles.inputt}>
+            <TextInput
+              style={styles.textInput}
+              onChangeText={setUsername}
+              value={userName}
+            />
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => setUserNameCreated(true)}>
+              <Text style={styles.chatButton}>Devam Et</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      ) : (
+        <ScrollView>
+          <View>
+            <View>
+              {messageList.map(message => {
+                return (
+                  <View>
+                    <Text style={styles.chatt}>
+                      {message.userName} {message.message}
+                    </Text>
+                    <Text style={styles.time}>{message.time}</Text>
+                  </View>
+                );
+              })}
+            </View>
+
+            <Text style={styles.chatText}>Chat </Text>
+            <View style={styles.inputt}>
+              <TextInput
+                style={styles.textInput}
+                onChangeText={setMessage}
+                value={message}
+              />
+              <TouchableOpacity
+                style={styles.button}
+                onPress={() => {
+                  socket.emit('event', {message: message, userName: userName});
+                }}>
+                <Text style={styles.chatButton}>Gönder</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </ScrollView>
+      )}
     </SafeAreaView>
   );
 };
@@ -52,12 +115,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+
   chatext: {
-    fontSize: 30,
+    fontSize: 20,
     fontWeight: 'bold',
     color: '#ffffff',
     marginTop: 230,
     marginLeft: 10,
+  },
+  time: {
+    alignSelf: 'flex-end',
+    marginRight: 9,
+    color: '#ffffff'
   },
   imagecontainer: {
     height: height,
@@ -70,24 +139,29 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     borderColor: 'white',
     padding: 10,
+    color: '#ffffff',
   },
+  chatText: {fontSize: 20, fontWeight: 'bold', color: 'white', marginLeft: 8},
   chatButton: {
     textAlign: 'center',
     fontWeight: 'bold',
     color: '#6320EE',
     fontSize: 20,
   },
+  chatt: {
+    color: 'white',
+  },
   button: {
     backgroundColor: 'white',
     width: 120,
-    height: 50,
+    height: 40,
     alignSelf: 'flex-end',
     marginRight: 13,
     justifyContent: 'center',
-    borderRadius: 5,
+    borderRadius: 7,
     marginLeft: 160,
     borderWidth: 1,
-    margin:5,
+    margin: 5,
     borderColor: 'white',
   },
   ınputt: {
